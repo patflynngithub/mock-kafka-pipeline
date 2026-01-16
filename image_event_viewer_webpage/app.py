ff0cad363be283fbd4d839c6fe80522f9984b765
Patrick Flynn <patflynn@hotmail.com>
2026-01-15 20:59:53 -0800

    Clean up webpage image event viewer and add doc about it to README.md. The webpage code still needs to check if the entered alert number is actually an exixsting one in the image pipeline

--- image_event_viewer_webpage/app.py
+++ image_event_viewer_webpage/app.py
@@ -1,20 +1,34 @@
+"""
+This is an image event viewer for the image pipeline, 
+displaying a webpage to allow viewing an image event's
+involved images.
+
+It uses the python Flask framework, which allows having
+dynamic webpages via server-side python scripting.
+
+None of Flask's nice organizational and management tools are used here.
+For now, I want to stay close to the low-level for generating
+dynamic webpages so that I can renew and extend my basic front-end
+skills.
+"""
+
 import os
 
 from flask import Flask, request, send_from_directory
 
-# Create an instance of the Flask class
 app = Flask(__name__)
 
-# Define the absolute path to your image directory
-# Replace 'path/to/your/images' with the actual path
-# IMAGE_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'path/to/your/images')
+# Absolute path to the image pipeline main directory inside the container
+# (via using the -v argument with the docker run command).
+# The images and the image event and image event alert info text files are in 
+# subdirectories of this directory.
 IMAGE_FOLDER = "/pipeline"
 
 # ----------------------------------------------------------------------------------
 
 def extract_path(a_string):
 
-    """ Extract the "outside of the docker container" path. """
+    """ Extract the path from the string. """
 
     start_wanted_path = a_string.find("/pipeline")
     wanted_path = a_string[start_wanted_path:-1]
@@ -25,7 +39,7 @@ def extract_path(a_string):
 
 def get_image_paths(image_event_alert_path):
 
-    """ Use Image event alert file to get image paths from image event file. """
+    """ Use image event alert file to get image paths from image event file. """
 
     image_path      = None
     diff_image_path = None
@@ -52,7 +66,7 @@ def get_image_paths(image_event_alert_path):
             lines = f.readlines()
     
         for line in lines:
-            line = line.strip()  # strip() removes leading/trailing whitespace, including \n
+            line = line.strip()
 
         image_path      = extract_path(lines[2])
         prev_image_path = extract_path(lines[3])
@@ -66,15 +80,14 @@ def get_image_paths(image_event_alert_path):
 
     return image_path, diff_image_path, prev_image_path
 
-# ----------------------------------------------------------------------------------------
-
-@app.route('/pipeline/<path:filename>')
-def serve_image(filename):
-    return send_from_directory(IMAGE_FOLDER, filename)
-
 # ----------------------------------------------------------------------------
 
 def get_image_event_imgs(alert_num_str):
+    
+    """
+    Using the image event alert number, the path to the image event alert info text file
+    is generated, which is used to get the image event image paths.
+    """
 
     alert_num = int(alert_num_str)
     image_event_alert_path = f"/pipeline/image_event_alerts/image_event_alert_{alert_num:03d}.txt"
@@ -85,20 +98,38 @@ def get_image_event_imgs(alert_num_str):
 
 # ----------------------------------------------------------------------------------------
 
-# Use the route() decorator to tell Flask what URL should trigger the function
+@app.route('/pipeline/<path:filename>')
+def serve_image(filename):
+
+    """
+    Send webpage requested image to the web browser
+    """
+    return send_from_directory(IMAGE_FOLDER, filename)
+
+# ----------------------------------------------------------------------------------------
+
 @app.route('/')
 def display_page():
 
+    """
+    Dislays the image event alert number entry dynamic webpage. 
+    It handles two states of the webpage. First is the initial
+    accepting of input of an image event alert number. Second is
+    the display of the image event images associated with the
+    input alert number.
+    """
+
     # note extra empty line to end the headers
-    # webpage = "Content-Type: text/html\n\n"
     webpage = "<!DOCTYPE html>\n\n"
 
-    # Now print the HTML content
+    # Collect the HTML content
+
     webpage += "<html>\n"
     webpage += "<head><title>Image Event Viewer</title></head>\n"
     webpage += "<body>\n"
 
-    webpage += '<h1 style="text-align:center;">Enter an image event alert number (positive integer)</h1>\n'
+    webpage += '<h2 style="text-align:center;">Enter an image event alert number</h2>\n'
+    webpage += '<h5 style="text-align:center;">(positive integer)</h5>\n'
     webpage += '<form style="text-align:center;" action="http://127.0.0.1:8000">\n'
     webpage += '   <label for="alert_num">Alert number:</label><br>\n'
     webpage += '   <input type="text" id="alert_num" name="alert_num"><br>\n'
@@ -108,9 +139,13 @@ def display_page():
     alert_num = request.args.get('alert_num','')
     if alert_num:
         if not alert_num.isdecimal():
-            webpage += f"<p>Alert number not a decimal': {alert_num}</p>\n"
+            webpage += f'<p style="text-align:center;">Alert number needs to be a positive integer: \"{alert_num}\" entered</p>\n'
+
+        # a positive integer number was input
         else:
-            webpage += f"<p>Alert number: {alert_num}</p>\n"
+            # Display the images of the image event
+
+            webpage += f'<p style="text-align:center;">Alert number: {alert_num}</p>\n'
             image_path, diff_image_path, prev_image_path = get_image_event_imgs(alert_num)
 
             webpage  +=  '<div style="display: flex; justify-content: center; gap: 20px;" class="image_and_prev_image">\n'
@@ -135,11 +170,9 @@ def display_page():
     webpage += "</html>\n"
 
     return webpage
-    # return f'<p>Hello, World!</p><p>__name__={__name__}</p>'
 
 # ==================================================================
 
-# Run the application
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=True)
 
--- image_event_viewer_webpage/set_up_docker.sh
+++ image_event_viewer_webpage/set_up_docker.sh
@@ -1,9 +1,15 @@
+#!/usr/bin/env bash
 
-# Clean up docker environment of containers and images
+# Clean up any previous image event viewer webpage docker images and containers
 docker rm python-flask-server_container
 docker rmi python-flask-server_image:latest
 
-# Set up image viewer webpage docker image/container
+# Set up image event viewer Flask webpage docker image/container
+
 docker build --progress=plain -t python-flask-server_image .
+
+# the .. in the -v argument below references the image pipeline's main application directory;
+# allows the Flask application in the container to access the data file results of the most 
+# recent image pipeline run
 docker run -v ..:/pipeline -p 8000:8000 --name python-flask-server_container python-flask-server_image
 
